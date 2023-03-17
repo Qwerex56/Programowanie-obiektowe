7940740522cc152da4138a3c9813f1c7c9a70c1d
Adrian <adrianczubaty.prywatny@gmail,com>
2023-03-17 08:41:29 +0100

    File name refactor

---
+++ Lista1i2/plansza.cpp
@@ -0,0 +1,100 @@
+#include "plansza.h"
+
+#ifdef PLANSZA_H
+
+Board::Board() {
+  board = CreteBoard();
+}
+
+Board::Board(const Board &b){
+  width = b.width;
+  height = b.height;
+
+  board = CreteBoard();
+  for (size_t y = 0; y < height; y++) {
+    for (size_t x = 0; x < width; x++) {
+      board[y][x] = b.board[y][x];
+    }
+  }
+}
+
+Board::Board(int w, int h) {
+  width = w;
+  height = h;
+
+  board = CreteBoard();
+}
+
+Board::~Board() {
+  DeleteBoard();
+}
+
+const Board& Board::operator=(const Board &b) {
+  if (this == &b)
+    return *this;
+
+  DeleteBoard();
+
+  width = b.width;
+  height = b.height;
+
+  board = CreteBoard();
+  for (size_t y = 0; y < height; y++) {
+    for (size_t x = 0; x < width; x++) {
+      board[y][x] = b.board[y][x];
+    }
+  }
+
+  return *this;
+}
+
+constexpr int Board::Width() const {
+  return width;
+}
+
+constexpr int Board::Height() const {
+  return height;
+}
+
+constexpr int Board::Get(int x, int y) const {
+  if (x >= width || y >= height || x < 0 || y < 0)
+    throw std::runtime_error("X or Y out of array bounds");
+
+  return board[y][x];
+}
+
+void Board::Set(int x, int y, int val) {
+  try {
+    Get(x, y);
+  }
+  catch(const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return;
+  }
+
+  board[y][x] = val;
+}
+
+int** Board::CreteBoard() const {
+  int** board = new int *[height];
+  for (size_t i = 0; i < height; i++) {
+    board[i] = new int[width];
+  }
+
+  for (size_t y = 0; y < height; y++) {
+    for (size_t x = 0; x < width; x++) {
+      board[y][x] = 0;
+    }
+  }
+
+  return board;
+}
+
+void Board::DeleteBoard() const {
+  for (size_t y = height - 1; y >= 0; y--) {
+    delete[] board[y];
+  }
+
+  delete[] board;
+}
+#endif
--- Lista1i2/plansza.h
+++ Lista1i2/plansza.h
@@ -6,8 +6,11 @@
 class Board {
 public:
   Board();
+  Board(const Board &);
   Board(int, int);
   ~Board();
+
+  const Board& operator=(const Board &);
 
   constexpr int Width() const;
   constexpr int Height() const;
@@ -16,8 +19,8 @@ public:
   constexpr int Get(int, int) const;
 
   friend std::ostream& operator<<(std::ostream& os, Board& b) {
-    for (size_t y = 0; y < b.width; y++) {
-      for (size_t x = 0; x < b.height; x++) {
+    for (size_t y = 0; y < b.height; y++) {
+      for (size_t x = 0; x < b.width; x++) {
         os << b.Get(x, y) << " ";
       }
       os << std::endl;
@@ -29,6 +32,9 @@ private:
   int **board;
   int height = 1; //x
   int width  = 1; //y
+
+  int **CreteBoard() const;
+  void DeleteBoard() const; // Use with Create Board
 };
 
 #endif
