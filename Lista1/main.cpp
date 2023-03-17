bf0fa6110355a45ad0ff631310f6c3093a9b05b4
Adrian <adrianczubaty.prywatny@gmail,com>
2023-03-17 08:40:42 +0100

    Updated by copy constructor and copy assignemnt

--- Lista1/main.cpp
+++ Lista1/main.cpp
@@ -4,17 +4,25 @@
 #include "plansza.h"
 
 int main() {
-  Board plansza(3, 2); //y x
+  Board p1(3, 2); //y x
 
-  std::cout << plansza << std::endl;
+  std::cout << p1 << std::endl;
 
-  std::cout << std::endl;
+  p1.Set(0, 1, 10);
+  p1.Set(1, 0, 20);
+  p1.Set(2, 1, 30);
 
-  plansza.Set(0, 0, 20);
-  plansza.Set(1, 1, 10);
-  plansza.Set(1, 2, 30);
-  std::cout << plansza;
-  
+  std::cout << p1;
+
+  Board p2(p1);
+  std::cout << std::endl << p2;
+
+  Board p3;
+  p3 = (p2 = p1);
+  std::cout << p3.Get(0, 0);
+  std::cout << p3 << std::endl;
+  std::cout << p3.Get(0, 0);
+
   _getch();
   return 0;
 }
